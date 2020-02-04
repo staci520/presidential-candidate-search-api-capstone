9021c54558e140fea6988a17919087a9d733c48e
Staci Stark <staci520@yahoo.com>
2020-02-03 17:55:38 -0800

    modify readme file

--- README.md
+++ README.md
@@ -1,3 +1,69 @@
 # presidential-candidate-search-api-capstone
 
-Live preview at https://staci520.github.io/presidential-candidate-search-api-capstone/
+# **2020 U.S. Presidential Election … in so many words** #
+
+An application that makes the election more manageable by easily getting information that matters in one place. Research and connect with the presidential candidates in a few clicks. The app also includes links to current election poll data, voter registration status, and five recent Politifact Truth-O-Meter reviews.
+
+## **Demo** ##
+Live preview at "https://staci520.github.io/presidential-candidate-search-api-capstone/"
+
+## **Motivation** ##
+
+The 2020 U.S. presidential election is a hot topic this year. With so much information and so little time to make informed decisions, this app allows users access to key information not only from the candidates but also from the media. A user can easily confirm whether they are registered to vote and, if not, learn how to register.
+
+## **Features** ##
+
+Select current leading or former presidential candidates. Upon selecting a candidate, three categories of data are loaded for which the user may interact with.  
+
+**Connect** loads a number of links for the selected candidate as available:  
+<ul>
+ <li>VoteSmart link to data</li>
+ <li>Campaign website</li>
+ <li>Email(s)</li>
+ <li>YouTube</li> 
+ <li>Twitter</li>
+ <li>Facebook</li> 
+ <li>Instagram</li>
+ <li>LinkedIn</li> 
+ <li>Medium</li>
+ <li>Flickr</li>
+ </ul>
+
+**Read**  loads a number of news articles, including top headlines, from multiple sources regarding the selected candidate.
+
+**Watch**  loads 5 YouTube videos regarding the selected candidate.
+
+## **Technologies** ## 
+<ul>
+ <li>HTML5</li>
+ <li>CSS</li>
+ <li>JavaScript</li>
+ <li>jQuery</li> 
+ </ul>
+
+## **Responsive** ##
+
+This app is built to be usable on mobile devices, as well as responsive across tablet, laptop, and desktop screen resolutions.
+
+## **Resources** ##
+<ul>
+ <li>YouTube API</li>
+ <li>VoteSmart API</li>
+ <li>NewApi.org</li> 
+ <li>Rock The Vote widget</li>
+ <li>Politifact widget</li>
+</ul>
+
+## **Screenshots** ## 
+
+![alt text](/screenshots/LandingPage.png)
+
+![alt text](/screenshots/CurrentCandidates.png)
+
+![alt text](/screenshots/FormerCandidates.png)
+
+![alt text](/screenshots/RealClearPolitics.png)
+
+![alt text](/screenshots/Politifact.png)
+
+![alt text](/screenshots/Registration.png)
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
 
-
-
-
+//format query parameters for the URL
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
@@ -11,8 +9,9 @@ function formatQueryParams(params) {
 
 //VOTESMART API
 
+//function to call Vote Smart API by candidate name
 function getVoteSmart(query, candidateName) {
-  const apiKey = "6180b80d1ce369c999188893e5e264ec"
+  const apiKey = '6180b80d1ce369c999188893e5e264ec'
   const searchURL = 'https://api.votesmart.org/Address.getCampaignWebAddress';
   // const searchURL2 = 'http://api.votesmart.org/CandidateBio.getBio';
   const params = {
@@ -23,8 +22,7 @@ function getVoteSmart(query, candidateName) {
   const queryString = formatQueryParams(params)
   const url = searchURL + '?' + queryString + '&o=JSON';
 
-  console.log(url);
-
+  //fetch information from API
   fetch(url)
     .then(response => {
       if (response.ok) {
@@ -34,80 +32,81 @@ function getVoteSmart(query, candidateName) {
     })
     .then(responseJson => displayVoteSmart(responseJson, candidateName))
     .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      $('#js-votesmart-error-message').text(`Something went wrong: ${err.message}`);
     });
 }
 
-//Display VoteSmart
+//function to display VoteSmart results
 
 function displayVoteSmart(responseJson, candidateName) {
   // if there are previous results, remove them
-  console.log("VS ====>", responseJson);
+  console.log('VS ====>', responseJson);
   console.log(typeof responseJson.error);
 
   $('#results-VoteSmart-list').empty();
   $('#candidateName').empty();
 
+  //loop to search for current and former candidates
   if (typeof responseJson.error === 'object') {
     console.log(`Campaign Web address for ${candidateName} no longer available.`)
-    $("#candidateName").replaceWith(`<b><h5 id="candidateName">Campaign web address for ${candidateName} no longer available.</h5></b>`)
+    $('#candidateName').replaceWith(`<b><h5 id='candidateName'>Campaign web address for ${candidateName} no longer available.</h5></b>`)
     return;
   }
 
   for (let obj in responseJson) {
     console.log(`VoteSmart Bio: ${responseJson[obj].generalInfo.linkBack}`)
-    $("#candidateName").replaceWith(`<b><h4 id="candidateName">${responseJson[obj].candidate.firstName} ${responseJson[obj].candidate.lastName}</h4></b>`)
+    $('#candidateName').replaceWith(`<b><h4 id='candidateName'>${responseJson[obj].candidate.firstName} ${responseJson[obj].candidate.lastName}</h4></b>`)
     $('#results-VoteSmart-list').append(
-      `<li><a href="${responseJson[obj].generalInfo.linkBack}" target="_blank"> <i class="fas fa-binoculars fa-4x"></i><span>VoteSmart</span></a></li>`)
+      `<li><a href='${responseJson[obj].generalInfo.linkBack}' target='_blank'> <i class='fas fa-binoculars fa-4x'></i><span>VoteSmart</span></a></li>`)
 
     for (let i = 0; i < responseJson[obj].address.length; i++) {
-      //console.log("VS Candidate Link ===>",responseJson[obj].address[i].webAddress)
+      //console.log('VS Candidate Link ===>',responseJson[obj].address[i].webAddress)
       const webAddressType = responseJson[obj].address[i].webAddressType;
       const webAddress = responseJson[obj].address[i].webAddress;
-      if (webAddressType === "Email") {
-        $('#results-VoteSmart-list').append(`<li><a href="mailto:${webAddress}" target="_blank"> 
-          <i class="fas fa-envelope-square fa-4x"></i><span>Mail</span></a></li>`)
+      if (webAddressType === 'Email') {
+        $('#results-VoteSmart-list').append(`<li><a href='mailto:${webAddress}' target='_blank'> 
+          <i class='fas fa-envelope-square fa-4x'></i><span>Mail</span></a></li>`)
 
-      } else if (webAddressType === "Webmail") {
-        $('#results-VoteSmart-list').append(`<li><a href="${webAddress}" target="_blank"> 
-          <i class="fas fa-envelope-square fa-4x"></i><span>Mail</span></a></li>`)
+      } else if (webAddressType === 'Webmail') {
+        $('#results-VoteSmart-list').append(`<li><a href='${webAddress}' target='_blank'> 
+          <i class='fas fa-envelope-square fa-4x'></i><span>Mail</span></a></li>`)
 
-      } else if (webAddressType === "Website - Twitter" || webAddress === "https://twitter.com/WayneMessam") {
-        $('#results-VoteSmart-list').append(`<li><a href="${webAddress}" target="_blank"> 
-          <i class="fab fa-twitter-square fa-4x"></i><span>Twitter</span></a></li>`)
+      } else if (webAddressType === 'Website - Twitter' || webAddress === 'https://twitter.com/WayneMessam') {
+        $('#results-VoteSmart-list').append(`<li><a href='${webAddress}' target='_blank'> 
+          <i class='fab fa-twitter-square fa-4x'></i><span>Twitter</span></a></li>`)
 
-      } else if (webAddressType === "Website - Facebook") {
-        $('#results-VoteSmart-list').append(`<li><a href="${webAddress}" target="_blank"> 
-          <i class="fab fa-facebook-square fa-4x"></i><span>Facebook</span></a></li>`)
+      } else if (webAddressType === 'Website - Facebook') {
+        $('#results-VoteSmart-list').append(`<li><a href='${webAddress}' target='_blank'> 
+          <i class='fab fa-facebook-square fa-4x'></i><span>Facebook</span></a></li>`)
 
-      } else if (webAddressType === "Website - Instagram") {
-        $('#results-VoteSmart-list').append(`<li><a href="${webAddress}" target="_blank"> 
-          <i class="fab fa-instagram fa-4x"></i><span>Instagram</span></a></li>`)
+      } else if (webAddressType === 'Website - Instagram') {
+        $('#results-VoteSmart-list').append(`<li><a href='${webAddress}' target='_blank'> 
+          <i class='fab fa-instagram fa-4x'></i><span>Instagram</span></a></li>`)
 
-      } else if (webAddressType === "Website - YouTube") {
-        $('#results-VoteSmart-list').append(`<li><a href="${webAddress}" target="_blank"> 
-          <i class="fab fa-youtube-square fa-4x"></i><span>YouTube</span></a></li>`)
+      } else if (webAddressType === 'Website - YouTube') {
+        $('#results-VoteSmart-list').append(`<li><a href='${webAddress}' target='_blank'> 
+          <i class='fab fa-youtube-square fa-4x'></i><span>YouTube</span></a></li>`)
 
-      } else if (webAddressType === "Website - LinkedIn") {
-        $('#results-VoteSmart-list').append(`<li><a href="${webAddress}" target="_blank"> 
-          <i class="fab fa-linkedin fa-4x"></i><span>LinkedIn</span></a></li>`)
+      } else if (webAddressType === 'Website - LinkedIn') {
+        $('#results-VoteSmart-list').append(`<li><a href='${webAddress}' target='_blank'> 
+          <i class='fab fa-linkedin fa-4x'></i><span>LinkedIn</span></a></li>`)
 
-      } else if (webAddress === "https://www.snapchat.com/add/bernie.sanders" || webAddress === "https://www.snapchat.com/add/GovernorBullock") {
-        $('#results-VoteSmart-list').append(`<li><a href="${webAddress}" target="_blank"> 
-              <i class="fab fa-snapchat-square fa-4x"></i><span>SnapChat</span></a></li>`)
+      } else if (webAddress === 'https://www.snapchat.com/add/bernie.sanders' || webAddress === 'https://www.snapchat.com/add/GovernorBullock') {
+        $('#results-VoteSmart-list').append(`<li><a href='${webAddress}' target='_blank'> 
+              <i class='fab fa-snapchat-square fa-4x'></i><span>SnapChat</span></a></li>`)
 
-      } else if (webAddress === "https://www.flickr.com/photos/146043801@N08/with/31817149657/") {
-        $('#results-VoteSmart-list').append(`<li><a href="${webAddress}" target="_blank"> 
-            <i class="fab fa-flickr fa-4x"></i><span>Flickr</span></a></li>`)
+      } else if (webAddress === 'https://www.flickr.com/photos/146043801@N08/with/31817149657/') {
+        $('#results-VoteSmart-list').append(`<li><a href='${webAddress}' target='_blank'> 
+            <i class='fab fa-flickr fa-4x'></i><span>Flickr</span></a></li>`)
 
-      } else if (webAddress === "https://medium.com/@Tom_Steyer" || webAddress === "https://medium.com/@TulsiGabbard" || webAddress === "https://medium.com/@KamalaHarris" || webAddress === "https://medium.com/@stevebullock") {
-        $('#results-VoteSmart-list').append(`<li><a href="${webAddress}" target="_blank"> 
-          <i class="fab fa-medium fa-4x"></i><span>Medium</span></a></li>`)
+      } else if (webAddress === 'https://medium.com/@Tom_Steyer' || webAddress === 'https://medium.com/@TulsiGabbard' || webAddress === 'https://medium.com/@KamalaHarris' || webAddress === 'https://medium.com/@stevebullock') {
+        $('#results-VoteSmart-list').append(`<li><a href='${webAddress}' target='_blank'> 
+          <i class='fab fa-medium fa-4x'></i><span>Medium</span></a></li>`)
 
-      } else  {
-        $('#results-VoteSmart-list').append(`<li><a href="${webAddress}" target="_blank"> 
-          <i class="fas fa-bullhorn fa-4x"></i><span>Website</span></a></li>`)
-      } 
+      } else {
+        $('#results-VoteSmart-list').append(`<li><a href='${webAddress}' target='_blank'> 
+          <i class='fas fa-bullhorn fa-4x'></i><span>Website</span></a></li>`)
+      }
     }
   };
   ;
@@ -116,13 +115,13 @@ function displayVoteSmart(responseJson, candidateName) {
 // NEWS API
 
 function getNews(query, maxResults = 2) {
-  const apiKey = "5d34f3b1e65347169f2b4a651b8fd8ea"
+  const apiKey = '5d34f3b1e65347169f2b4a651b8fd8ea'
   const searchURL1 = 'https://newsapi.org/v2/top-headlines';
   const searchURL2 = 'https://newsapi.org/v2/everything'
   const params = {
     q: query,
-    language: "en",
-  }; 
+    language: 'en',
+  };
   const queryString = formatQueryParams(params)
   const url1 = searchURL1 + '?' + queryString;
   const url2 = searchURL2 + '?' + queryString;
@@ -132,7 +131,7 @@ function getNews(query, maxResults = 2) {
 
   const options = {
     headers: new Headers({
-      "X-Api-Key": apiKey
+      'X-Api-Key': apiKey
     })
   };
 
@@ -146,9 +145,10 @@ function getNews(query, maxResults = 2) {
     })
     .then(responseJson => displayNews(responseJson, maxResults))
     .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      $('#js-news-error-message').text(`Something went wrong: ${err.message}`);
     });
-  //fetch everything
+
+  //fetch everything (all news)
   fetch(url2, options)
     .then(response => {
       if (response.ok) {
@@ -158,11 +158,11 @@ function getNews(query, maxResults = 2) {
     })
     .then(responseJson => displayNews(responseJson, maxResults))
     .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      $('#js-news-error-message').text(`Something went wrong: ${err.message}`);
     });
 }
 
-//Display News API
+//function to display News API results
 
 function displayNews(responseJson, maxResults) {
   // if there are previous results, remove them
@@ -172,7 +172,7 @@ function displayNews(responseJson, maxResults) {
   for (let i = 0; i < responseJson.articles.length & i < maxResults; i++) {
     let publishedAt = responseJson.articles[i].publishedAt.substring(0, 10);
     $('#results-News-list').append(
-      `<li><h4><a href="${responseJson.articles[i].url}" target="_blank" >${responseJson.articles[i].title}</a></h4><br>
+      `<li><h4><a href='${responseJson.articles[i].url}' target='_blank' >${responseJson.articles[i].title}</a></h4><br>
       <p>${responseJson.articles[i].source.name} by ${responseJson.articles[i].author}, ${publishedAt}</p><br>
       <p><img src='${responseJson.articles[i].urlToImage}'>
       ${responseJson.articles[i].description}</p>
@@ -182,6 +182,8 @@ function displayNews(responseJson, maxResults) {
 };
 
 // YOUTUBE
+
+//function to get YouTube videos of candidate clicked
 
 function getYouTubeVideos(candidateName) {
 
@@ -208,11 +210,13 @@ function getYouTubeVideos(candidateName) {
     })
     .then(responseJson => displayYouTubeVideos(responseJson))
     .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      $('#js-youtube-error-message').text(`Something went wrong: ${err.message}`);
     });
 }
 
 //Display YouTube
+
+//function to display results
 
 function displayYouTubeVideos(responseJson) {
   // if there are previous results, remove them
@@ -225,7 +229,7 @@ function displayYouTubeVideos(responseJson) {
     console.log(publishedAt);
     console.log(youTubeUrl);
     $('#results-YouTube-list').append(
-      `<li><h4><a href="${youTubeUrl}" target="_blank"> ${responseJson.items[i].snippet.title}</a></h4><br>
+      `<li><h4><a href='${youTubeUrl}' target='_blank'> ${responseJson.items[i].snippet.title}</a></h4><br>
       <p>${publishedAt}</p>
       <p><img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
       ${responseJson.items[i].snippet.description}</p>
@@ -235,7 +239,7 @@ function displayYouTubeVideos(responseJson) {
 };
 
 
-//Watch click 
+//function to watch user clicks 
 
 function watchCandidate() {
 
@@ -251,18 +255,18 @@ function watchCandidate() {
     $('.results-container').get(0).scrollIntoView()
     getVoteSmart(obj.candidateId, candidateName)
     getYouTubeVideos(candidateName);
-    // alert("Candidate Clicked!")
+    // alert('Candidate Clicked!')
     getNews(candidateName)
   });
 }
 
 $(watchCandidate);
 
-//Hover effect on image 
+//function that creates hover effect on selected image 
 
 $(document).ready(function () {
-  $(".Image").css("opacity", 1.0);
-  $(".Image").hover(function () {
+  $('.Image').css('opacity', 1.0);
+  $('.Image').hover(function () {
     $(this).animate({ opacity: 0.65 }, 500);
   }, function () {
     $(this).animate({ opacity: 1.0 }, 500);
